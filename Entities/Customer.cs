887f6f3120983582fac795daf3a3eb7b31c0aa4c
Ida Fahler <ida@fahler.se>
2026-04-02 13:14:24 +0200

    changed customer entity

--- Entities/Customer.cs
+++ Entities/Customer.cs
@@ -8,7 +8,7 @@ public class Customer
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public string SSN { get; set; }
+    public DateTime BirthDate { get; set; }
     public string Email { get; set; }
     public string Phone { get; set; }
     public ICollection<Address> Addresses { get; set; } = new List<Address>();
