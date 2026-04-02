7a64b657523c63a7f89cf182d16161d04a08dfd8
Ida Fahler <ida@fahler.se>
2026-04-02 13:49:29 +0200

    created context file and appsettings with connection string

---
+++ EFCore/WebshopDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCore
+{
+    internal class WebshopDbContext : DbContext
+    {
+        public DbSet <Address> Adresses { get; set; }
+        public DbSet <Brand> Brands { get; set; }
+        public DbSet <Category> Categories { get; set; }
+        public DbSet <Customer> Customers { get; set; }
+        public DbSet <Order> Orders { get; set; }
+        public DbSet <OrderProduct> OrderProducts { get; set; }
+        public DbSet <Payment> Payments { get; set; }
+        public DbSet <Product> Products { get; set; }
+        public DbSet <Shipping> Shippings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("Finnish_Swedish_CI_AS");
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString =
+                config.GetConnectionString("DefaultConnection");
+            
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
