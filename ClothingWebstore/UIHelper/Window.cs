35004421c88035f85b800a2207e196af7e61104c
Linda <253105283+LindaMHK@users.noreply.github.com>
2026-04-03 09:54:15 +0200

    Adding window class

---
+++ ClothingWebstore/UIHelper/Window.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClothingWebstore.UIHelper
+{
+    public class Window
+    {
+        public string Header { get; set; }
+        public int Left { get; set; }
+        public int Top { get; set; }
+        public List<string> TextRows { get; set; }
+
+        public Window(string header, int left, int top, List<string> textRows)
+        {
+            Header = header;
+            Left = left;
+            Top = top;
+            TextRows = textRows;
+        }
+
+        public void Draw()
+        {
+            var width = TextRows.OrderByDescending(s => s.Length).FirstOrDefault().Length;
+
+
+            if (width < Header.Length + 4)
+            {
+                width = Header.Length + 4;
+            };
+
+            Console.SetCursorPosition(Left, Top);
+            if (Header != "")
+            {
+                Console.Write('┌' + " ");
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.Write(Header);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(" " + new String('─', width - Header.Length) + '┐');
+            }
+            else
+            {
+                Console.Write('┌' + new String('─', width + 2) + '┐');
+            }
+
+
+            for (int j = 0; j < TextRows.Count; j++)
+            {
+                Console.SetCursorPosition(Left, Top + j + 1);
+                Console.WriteLine('│' + " " + TextRows[j] + new String(' ', width - TextRows[j].Length + 1) + '│');
+            }
+
+
+            Console.SetCursorPosition(Left, Top + TextRows.Count + 1);
+            Console.Write('└' + new String('─', width + 2) + '┘');
+
+
+
+            if (Lowest.LowestPosition < Top + TextRows.Count + 2)
+            {
+                Lowest.LowestPosition = Top + TextRows.Count + 2;
+            }
+
+            Console.SetCursorPosition(0, Lowest.LowestPosition);
+        }
+    }
+
+    public static class Lowest
+    {
+        public static int LowestPosition { get; set; }
+    }
+}
+
