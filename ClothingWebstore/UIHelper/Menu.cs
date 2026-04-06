f84fa8043455f54e35eeab20c9558efcbceb8d90
Ida Fahler <ida@fahler.se>
2026-04-06 13:46:38 +0200

    new name for existing method and new menu for admin start

--- ClothingWebstore/UIHelper/Menu.cs
+++ ClothingWebstore/UIHelper/Menu.cs
@@ -6,13 +6,27 @@ namespace ClothingWebstore.UIHelper
 {
     internal class Menu
     {
-        public static string ReturnStartMenu()
+        public static string ReturnGeneralStartMenu()
         {
             return """
                 Are you a customer / admin?
                 [C] Customer
                 [A] Admin
                 [Q] Quit
+
+                """;
+        }
+        public static string ReturnAdminStartMenu()
+        {
+            return """
+                What would you like to manage?
+                Press key + enter.
+                [1] Products
+                [2] Categories
+                [3] Customer
+                [4] Statistics
+
+                [B] Back
 
                 """;
         }
