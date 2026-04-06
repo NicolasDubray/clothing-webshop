e7f4c45caa40abfc3b92cd6d39fc4c3c68128852
Ida Fahler <ida@fahler.se>
2026-04-06 13:47:20 +0200

    admin starting menu and handling input

--- ClothingWebstore/AdminProgram.cs
+++ ClothingWebstore/AdminProgram.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClothingWebstore.UIHelper;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,6 +8,58 @@ namespace ClothingWebstore
     public class AdminProgram
     {
         public static void RunAdmin()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine(Menu.ReturnAdminStartMenu());
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        ManageProducts();
+                        break;
+
+                    case "2":
+                        ManageCategories();
+                        break;
+
+                    case "3":
+                        ManageCustomers();
+                        break;
+
+                    case "4":
+                        SeeStatistics();
+                        break;
+
+                    case "B":
+                    case "b":
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid.");
+                        Thread.Sleep(1000);
+                        break;
+                }
+            }
+        }
+        private static void ManageProducts()
+        {
+
+        }
+
+        private static void ManageCategories()
+        {
+
+        }
+
+        private static void ManageCustomers()
+        {
+
+        }
+
+        private static void SeeStatistics()
         {
 
         }
