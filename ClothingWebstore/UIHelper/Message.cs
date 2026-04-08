2e2575d33cbfb0f08f4c6435d60f98c3b3b49a90
Ida Fahler <ida@fahler.se>
2026-04-08 09:45:43 +0200

    adding class with ui help methods

---
+++ ClothingWebstore/UIHelper/Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClothingWebstore.UIHelper
+{
+    internal class Message
+    {
+        internal static void InvalidInput()
+        {
+            Console.WriteLine("Invalid. Try again.");
+            Thread.Sleep(1000);
+        }
+
+        internal static void PressAnyKeyToContinue()
+        {
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey(true);
+        }
+    }
+}
