2872b2d1fa5e855650154586ff485a144ceeafe5
Ida Fahler <ida@fahler.se>
2026-04-08 09:24:35 +0200

    Class with validating methods for customer properties

---
+++ ClothingWebstore/UIHelper/ValidateInput.cs
@@ -0,0 +1,33 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ClothingWebstore.UIHelper
+{
+    internal class ValidateInput
+    {
+        internal static bool IsValidId(string input, List<Customer> customers) =>
+            int.TryParse(input, out int id) && customers.Any(c => c.Id == id);
+
+
+        internal static bool IsValidName(string name) =>
+            name.Length > 0 && name.Length < 64
+            && name.All(c => char.IsLetter(c) || c == ' ');
+
+
+        internal static bool IsValidBirthDate(string birthDate) =>
+            DateTime.TryParseExact(birthDate, "yyyy-MM-dd", null, DateTimeStyles.None, out _);
+
+
+        internal static bool IsValidEmail(string email) =>
+            email.Length > 4 && email.Length <= 64
+            && email.IndexOf('@') > 0
+            && email.IndexOf('@') < email.Length - 1;
+
+
+        internal static bool IsValidPhone(string phone) =>
+            phone is not null && phone.Length == 10 && phone.All(char.IsDigit);
+    }
+}
