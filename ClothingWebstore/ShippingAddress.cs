2f8b4cdc16a74675e9aa15a82f93f5352ea9feee
Nicolas Dubray <nicolas.georges.dubray@gmail.com>
2026-04-14 14:07:28 +0200

    add cart management and checkout process

---
+++ ClothingWebstore/ShippingAddress.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClothingWebstore
+{
+    internal class ShippingAddress
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
